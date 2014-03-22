9ba67908a54afef971bd6a70cfc1bc1a7cea795d
chznight <element.chz@gmail.com>
2014-03-22 06:07:50 -0400

    added rest of the files

---
+++ server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# server.sh
+ECE419_HOME=/cad2/ece419s/
+JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
+
+# arguments to MazewarServer
+# $1 = number of player
+
+${JAVA_HOME}/bin/java MazewarServer $1 $2
+
+
+
+
+
+
