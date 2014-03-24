9010349a4e3097429cfd23deeaedf19b42a124a7
ssang92 <lsalander92@outlook.com>
2014-03-24 09:30:37 -0400

    Updated parameter description

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
+# $1 = NS ip
+# $2 = NS port
+# $3 = port
+# $4 = number of players (Do we need this?)
 
 ${JAVA_HOME}/bin/java Mazewar $1 $2 $3 $4
