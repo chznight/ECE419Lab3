2b69ab97a82547982970c0f04fb9459b8768e490
ssang92 <lsalander92@outlook.com>
2014-03-24 09:15:00 -0400

    Updated parameter description

--- naming_service.sh
+++ naming_service.sh
@@ -1,6 +1,7 @@
 JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
 
 # arguments to MazewarServer
-# $1 = number of player
+# $1 = port
+# $2 = number of player
 
 ${JAVA_HOME}/bin/java NamingService $1 $2
